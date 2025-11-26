e91886d957e087027a53255aa499e519ecfa8788
Marco Salado <marcousg@ciencias.unam.mx>
2025-11-26 12:14:12 -0600

    dataset

--- data/data.py
+++ data/data.py
@@ -26,36 +26,6 @@ data_assets.columns = [
 returns = np.log(data_assets).diff().dropna()
 returns.columns = [c + "_ret" for c in returns.columns]
 
-# -----------------------------------------------
-# 3. Descarga de Features Macro y de Mercado
-# (vía yfinance: “^TNX” = 10Y, etc.)
-# -----------------------------------------------
-
-
-feature_tickers = ["^IRX", "^TNX", "^TYX", "^VIX"]
-
-data_tickers = yf.download(
-    feature_tickers,
-    start="2013-01-01",
-    end="2025-01-01",
-    interval="1d"
-)["Close"]
-
-data_tickers.columns = ["YC_3M", "YC_5Y", "YC_10Y", "VIX"]
-
-data_tickers.reindex(returns.index).ffill().bfill()  
-
-features = data_tickers / 100.0 # Convertir a decimal (tasas están en %)
-
-# -----------------------------------------------
-# 4. Unificación del dataset: R_h + F_h
-# -----------------------------------------------
-Dh = pd.concat([returns, features], axis=1).dropna()
-
-print("Dataset listo para CTGAN")
-print(Dh.head())
-print("\nDimensiones:", Dh.shape)
-
 # Guardar el dataset final en un archivo CSV
-output_path = "dataset_ctgan.csv"
-Dh.to_csv(output_path, index=True)
+output_path = "dataset.csv"
+returns.to_csv(output_path, index=True)
